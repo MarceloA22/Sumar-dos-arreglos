198da6295fd82b23b458242efa74bf1d770be4b0
JMBenitez-dev <105891765+JMBenitez-dev@users.noreply.github.com>
2022-05-28 01:11:19 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ let v1: number[] = new Array(6);
 let v2: number[] = new Array(6);
 let posicion: number, indice: number;
 
-for (indice = 0; indice < 6; indice++) {
-  posicion = Number(
+for (indice = 0; indice < 6; indice++) {// es conveniente usar v1.length ya que si tu cliente te pide que el array tenga mas elementos, solo necesitarias cambiar 
+  posicion = Number(                    //las lineas 1 y 2.
     prompt(
       `Ingrese el número que desea incorporar en la posición: ${indice}, del arreglo N°1: `
     )
@@ -19,7 +19,7 @@ for (indice = 0; indice < 6; indice++) {
   );
   v2[indice] = posicion;
 }
-let vSuma: number[] = [
+let vSuma: number[] = [// lo ideal es usar un ciclo para realizar la suma.
   v1[0] + v2[0],
   v1[1] + v2[1],
   v1[2] + v2[2],
@@ -30,3 +30,4 @@ let vSuma: number[] = [
 console.log("Valores ingresados del primer arreglo: ", v1);
 console.log("Valores ingresados del segundo arreglo: ", v2);
 console.log("suma de los arreglo 1 + arreglo 2 : ", vSuma);
+//esta correcta tu solucion
